6567b0c65eae537bc87a955174eb3cb579b80c17
Felipe Muller <felipeavmuller@gmail.com>
2021-06-13 21:14:07 -0300

    Alteraçao nos textos

--- stop-watch/src/components/TimerSettings.jsx
+++ stop-watch/src/components/TimerSettings.jsx
@@ -5,9 +5,9 @@ class TimerSettings extends React.Component {
     const { start, stop, reset } = this.props
     return (
       <div className='main-buttons-container'>
-        <button type='button' onClick={start} id='start'>COMEÇAR!</button>
-        <button type='button' onClick={stop} id='stop'>PARAR!!</button>
-        <button type='button' onClick={reset} id='reset'>RESETAR</button>
+        <button type='button' onClick={start} id='start'>START</button>
+        <button type='button' onClick={stop} id='stop'>STOP!</button>
+        <button type='button' onClick={reset} id='reset'>RESET</button>
       </div>
     );
   }
